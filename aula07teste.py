80af75afc18e8a09085f23e0a4d4398def0c9295
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2023-12-07 15:18:30 -0300

    Add files via upload

---
+++ aula07teste.py
@@ -0,0 +1,11 @@
+nota1 = int(input('digite sua primeira nota: '))
+nota2 = int(input('digite sua segunda nota: '))
+media = (nota1 + nota2) /2
+print('sua primeira nota é {}, sua segunda nota é {}, e a sua media é {}'.format(nota1, nota2, media))
+
+
+
+n1 = float(input('qual sua idade?'))
+n2 = float(input('qual a idade do seu pai?'))
+media = (n1 + n2) /2
+print('sua idade sendo {}, e a do seu pai sendo {}, a media entre a idade de vocês é {}!'.format(n1, n2, media))
