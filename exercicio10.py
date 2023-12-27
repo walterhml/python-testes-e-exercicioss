49fe4ef7f20d54c374ffdbf24decdb4a218f245c
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2023-12-27 16:26:13 -0300

    atividade

---
+++ exercicio10.py
@@ -0,0 +1,4 @@
+valor = float(input('quanto dinheiro você tem na carteira?'))
+dolarhoje = 4.83
+rasult = valor / dolarhoje
+print('voce tem {}, e consegue comprar {} isso de dolar'.format(valor, rasult))
