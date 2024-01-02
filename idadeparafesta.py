e40b358746be0184e2775b12e3afa18bfc20b447
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-01-02 13:23:19 -0300

    atividade

---
+++ idadeparafesta.py
@@ -0,0 +1,6 @@
+idade = float(input('Esta festa é para maiores de 18, quantos anos você tem?'))
+
+if idade > 18:
+    print('Voce pode entrar, curta a festa')
+else:
+    print('Infelizmente você nao podera curtir a festa')
